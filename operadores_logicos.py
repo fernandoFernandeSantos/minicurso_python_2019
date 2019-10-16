c9d8f03429ecf8a0aad8af97fbcfd43930863883
Your Name <you@example.com>
2019-10-16 11:13:12 -0300

    operadores logicos update

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -13,38 +13,33 @@ if ( a != b ):
 else:
    print("Linha 2 - a e igual a b")
 
-if ( a <> b ):
-   print("Linha 3 - a nao e igual a b")
-else:
-   print("Linha 3 - a e igual a b")
-
 if ( a < b ):
-   print("Linha 4 - a e menor que b") 
+   print("Linha 3 - a e menor que b") 
 else:
-   print("Linha 4 - a nao e menor que b")
+   print("Linha 3 - a nao e menor que b")
 
 if ( a > b ):
-   print("Linha 5 - a e maior que b")
+   print("Linha 4 - a e maior que b")
 else:
-   print("Linha 5 - a nao e maior que b")
+   print("Linha 4 - a nao e maior que b")
 
 a = 5
 b = 20
 if ( a <= b ):
-   print("Linha 6 - a e menor ou igual a b")
+   print("Linha 5 - a e menor ou igual a b")
 else:
-   print("Linha 6 - a maior que b")
+   print("Linha 5 - a maior que b")
 
 if ( b >= a ):
-   print("Linha 7 - b maior ou igual a b")
+   print("Linha 6 - b maior ou igual a b")
 else:
-   print("Linha 7 - b menor que b")
+   print("Linha 6 - b menor que b")
    
 # and, or e not
 falso = False
 verdadeiro = True
 
-print("Linha 8 - falso or verdadeiro {}".format(falso or verdadeiro))
-print("Linha 9 - falso and verdadeiro {}".format(falso and verdadeiro))
-print("Linha 10 - falso or not verdadeiro {}".format(falso or not verdadeiro)) 
+print("Linha 7 - falso or verdadeiro {}".format(falso or verdadeiro))
+print("Linha 8 - falso and verdadeiro {}".format(falso and verdadeiro))
+print("Linha 9 - falso or not verdadeiro {}".format(falso or not verdadeiro)) 
 
--- primo.py
+++ primo.py
@@ -1,6 +1,6 @@
 
 numero = input("Entre o numero para verificar: ")
-
+numero = int(numero)
 if numero > 1:
     for i in range(2, numero//2):
         if numero % i == 0:
