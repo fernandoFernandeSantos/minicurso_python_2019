29155bacac91f9be5315a6b2548ffbe49f5aa24b
Your Name <you@example.com>
2019-10-16 00:11:37 -0300

    aula update

---
+++ primo.py
@@ -0,0 +1,12 @@
+
+numero = input("Entre o numero para verificar: ")
+
+if numero > 1:
+    for i in range(2, numero//2):
+        if numero % i == 0:
+            print("{} nao e primo".format(numero))
+            break
+    else:
+        print("{} e primo".format(numero))
+else:
+    print("Nao e primo")
